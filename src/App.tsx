95c18dd66c264ab69922f7226028590bd02909b7
TecJP <tec.joao.freitas@gmail.com>
2023-01-10 21:04:59 -0300

    Improving the responsivity and some overflows

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { Header } from "./components/Header";
 import styles from './App.module.css';
 import "./global.css";
 import { NewTask } from "./components/NewTask";
-import { TasksTable } from "./components/TasksTable";
 
 export function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
-      <div className={styles.container}>
+      <div className={styles.containerTable}>
         <NewTask />
       </div>
     </div>
