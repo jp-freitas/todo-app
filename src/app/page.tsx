a22fb558de8507295370af52bdf8b040e163b6b4
jp-freitas <tec.joao.freitas@gmail.com>
2024-09-21 16:20:42 -0300

    dev: created the function to create the tasks and create a button to call the function.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+'use client'
+import { supabase } from "@/lib/supabase";
+//import { Todo } from "@/types/todo";
+
 export default function Home() {
+  async function createTodo() {
+    const {data, error} = await supabase
+      .from('todo')
+      .insert([
+        {
+          task: 'Teste'
+        }
+      ])
+      .select();
+    if (error) { console.error(error)
+    } else { console.log(data)};
+  }
+  
   return (
-    <div className="min-h-screen flex justify-center items-center">Hello World!</div>
+    <div className="min-h-screen flex justify-center items-center">
+      <button onClick={() => createTodo()} className="white-text hover:border-white h-2 border-2 border-neutral-950 py-6 px-8 flex justify-center items-center rounded-md">Create</button>
+    </div>
   );
 }
