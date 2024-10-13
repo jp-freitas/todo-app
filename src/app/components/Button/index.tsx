0c6d4409cb457c2dc6578a6196b09d79e7fdd8ed
jp-freitas <tec.joao.freitas@gmail.com>
2024-10-13 11:49:53 -0300

    dev: improving the button to use in others page.

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -8,7 +8,7 @@ export default function Button({ text, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className="text-neutral-400 font-semibold h-2 py-6 px-8 flex justify-evenly items-center rounded-md bg-neutral-800 hover:opacity-85"
+      className={`text-neutral-400 font-semibold h-2 py-6 px-8 flex justify-evenly items-center rounded-md bg-neutral-800 hover:opacity-85 ${rest.disabled && 'cursor-not-allowed opacity-75'}`}
     >
       {text}
     </button>
